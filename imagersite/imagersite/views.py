83021b81b55519d398d6103336af32311f8b9bb0
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-04-17 12:15:07 -0700

    home page redirects to profile page if user is logged it

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,13 +1,17 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from imager_images.models import Photo
-from django.conf import settings
 from django.contrib.staticfiles import finders
+from django.http import HttpResponseRedirect
 
 
 DEFAULT_IMAGE = finders.find('css/images/default-image')
+PROFILE = '/profile/'
+
 
 def home_page(request, *args, **kwargs):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(PROFILE)
     try:
         image = Photo.objects.filter(published="public").order_by("?")[0]
     except IndexError:
