4e0a59283c5644e70587a88749cca954ec02dd80
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-04-17 08:52:45 -0700

    beginnings of testing

---
+++ imagersite/imagersite/test_views.py
@@ -0,0 +1,58 @@
+from django.test import TestCase, Client
+from django.conf import settings
+from django.template.response import TemplateResponse
+from django.contrib.auth.models import User
+from imager_images.models import Photo
+
+
+DEFAULT_PIC = 'static/imagersite/images/default-image.jpg'
+HOME = '/'
+
+
+class EmptySite(TestCase):
+    """Views with no users in db."""
+
+    def setUp(self):
+        """Setup Unauthenticated User Tests."""
+        c = Client()
+        self.home_response = c.get(HOME)
+        self.home_context = self.home_response.context[0]
+
+    def test_no_users(self):
+        """Assert site created with no default users."""
+        self.assertFalse(User.objects.count())
+
+    def test_no_pictures(self):
+        """Assert site created with no user photos."""
+        self.assertFalse(Photo.objects.count())
+
+    def test_home_view_response_code(self):
+        """Assert that visiting home returns a 200 response code."""
+        self.assertEquals(self.home_response.status_code, 200)
+
+    def test_home_view_template(self):
+        templates = self.home_response.templates
+        self.assertEquals(templates[0].name, 'home.html')
+        self.assertEquals(templates[1].name, 'base.html')
+
+    def test_home_view_no_context(self):
+        """Assert that no image context is passed when no there are no photos."""
+        img_file = self.home_response.context['image']
+        self.assertIsNone(img_file)
+
+    def test_home_view_default_photo(self)
+
+
+class UnauthenticatedUser(TestCase):
+    """Views for unauthenticated users."""
+    # def test_home_view_random_photo(self):
+    #     """Assert that, if available, a random user photo appears on home."""
+    #     pass
+
+    pass
+
+
+class AuthenticatedUser(TestCase):
+    """Views for authenticated users."""
+
+    pass
--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from imager_images.models import Photo
+from django.conf import settings
 
 
 def home_page(request, *args, **kwargs):
