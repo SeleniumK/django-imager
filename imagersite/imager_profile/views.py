d23b04473e208aeb4bfa93c20c96f634bf673bf1
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-04-17 11:31:36 -0700

    username in nav bar, directs to profile

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
+class Profile(LoginRequiredMixin, TemplateView):
+    # template = 
+    pass
+    # @login_required(login_url='/login')
+
