b856697a64ad1a70c0ad4f33a96f20788fdedb46
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-04-17 11:44:25 -0700

    profile template and basic function style view for profile page

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.contrib.auth.mixins import LoginRequiredMixin
 
+PROFILE_TEMPLATE = 'profile.html'
+
+
+@login_required(login_url='/login')
+def Profile(request, *args, **kwargs):
+    """Authenticated User Profile."""
+    return render(request, PROFILE_TEMPLATE)
 
 # Create your views here.
-class Profile(LoginRequiredMixin, TemplateView):
-    # template = 
-    pass
-    # @login_required(login_url='/login')
+# class Profile(LoginRequiredMixin, TemplateView):
+    # """Authenticated User Profile."""
 
+    # template = 'profile.html'
+
+    # def authUserProfile(request)
