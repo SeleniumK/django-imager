ed7ac007e149afd04adddc310cd77387e7db35e2
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-04-19 11:58:06 -0700

    add and edit functions done. Can be done with same views

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -3,8 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from .models import Photo, Album
-from django.views.generic.edit import CreateView
-from django.forms import ModelForm
+from django.views.generic.edit import CreateView, ModelFormMixin, ProcessFormView
+from django.views.generic.detail import SingleObjectTemplateResponseMixin
+from django.views.generic import UpdateView
 import datetime
 
 LIBRARY_TEMPLATE = 'library.html'
@@ -30,50 +31,53 @@ def photo_view(request, **kwargs):
     return render(request, 'images/photo_view.html', context={'image': image})
 
 
-# class AlbumForm(ModelForm):
-#     """Album Form."""
-
-#     class Meta:
-#         """Meta."""
-
-#         model = Album
-#         fields = ["title", "description", "cover", "photos", "published"]
-
-#     def __init__(self, *args, **kwargs):
-#         """Damn you pep8."""
-#         super(AlbumForm, self).__init__(self, *args, **kwargs)
-#         self.fields['photos'].queryset = Photo.objects.filter(user=self.user)
-
-
-class AddContent(CreateView):
-    """Generic view for form to add new content."""
-
+class AddOrEditContent(SingleObjectTemplateResponseMixin, ModelFormMixin, ProcessFormView):
     template_name = 'forms.html'
     success_url = '/images/library'
+
+    def get_object(self, queryset=None):
+        try:
+            return super(AddOrEditContent, self).get_object(queryset)
+        except AttributeError:
+            return None
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super(AddOrEditContent, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
     def form_valid(self, form):
         """Set user to request user."""
         form.instance.user = self.request.user
         form.instance.date_published = datetime.datetime.now()
-        return super(AddContent, self).form_valid(form)
+        return super(AddOrEditContent, self).form_valid(form)
 
 
-class AddPhoto(AddContent):
+class AddOrEditPhoto(AddOrEditContent):
     """View to add new Photo."""
 
     model = Photo
     fields = ["title", "img_file", "description", "published"]
 
 
-class AddAlbum(AddContent):
+class AddOrEditAlbum(AddOrEditContent):
     """View for form to add new album."""
 
     model = Album
     fields = ["title", "description", "cover", "photos", "published"]
 
     def get_form(self, form_class):
-        form = super(AddAlbum, self).get_form(form_class)
+        """Set up field querysets."""
+        form = super(AddOrEditAlbum, self).get_form(form_class)
         user_photos = Photo.objects.filter(user=self.request.user)
         form.fields['cover'].queryset = user_photos
         form.fields['photos'].queryset = user_photos
         return form
+
