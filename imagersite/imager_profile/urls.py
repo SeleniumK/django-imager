f8cfb781a28b69345c06322a7e30108a14da9aa8
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-04-19 14:16:17 -0700

    Add edit profile view/url/template

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import Profile
+from .views import Profile, EditProfile
 
 urlpatterns = [
     url(r'^$', Profile, name="profile"),
+    url(r'^edit/', EditProfile.as_view(), name="edit")
 ]
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.views.generic.edit import UpdateView
+from .models import ImagerProfile
 
 PROFILE_TEMPLATE = 'profile.html'
 
@@ -7,5 +11,19 @@ PROFILE_TEMPLATE = 'profile.html'
 @login_required(login_url='/login')
 def Profile(request, *args, **kwargs):
     """Authenticated User Profile."""
-    import pdb; pdb.set_trace()
     return render(request, PROFILE_TEMPLATE)
+
+
+class EditProfile(UpdateView):
+    """Edit profile."""
+
+    model = ImagerProfile
+    fields = ['location', 'camera_type']
+    success_url = '/profile/'
+    template_name = 'edit_profile.html'
+
+    def get_object(self):
+        """Override get_object method."""
+        return get_object_or_404(User, pk=self.request.user.id)
+
+
