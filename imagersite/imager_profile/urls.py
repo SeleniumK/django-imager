f1ba2ff7cf036cce331f81136a474a12039ac3bf
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-04-19 16:52:44 -0700

    Add working edit profile view

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from .views import Profile, EditProfile
+from .views import Profile, edit_user_profile
 
 urlpatterns = [
     url(r'^$', Profile, name="profile"),
-    url(r'^edit/', EditProfile.as_view(), name="edit")
+    url(r'^edit/', edit_user_profile, name="edit")
 ]
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 from django.views.generic.edit import UpdateView
 from .models import ImagerProfile
 
@@ -14,16 +15,43 @@ def Profile(request, *args, **kwargs):
     return render(request, PROFILE_TEMPLATE)
 
 
-class EditProfile(UpdateView):
-    """Edit profile."""
+def edit_user_profile(request):
+    """Edit user profile."""
+    current_user = User.objects.get(pk=request.user.id)
+    current_profile = current_user.profile
+    if request.method == 'POST':
+        form2 = EditUser(request.POST, instance=current_user)
+        form1 = EditProfile(request.POST, instance=current_profile)
+        if form1.is_valid() and form2.is_valid():
+            form1.save()
+            form2.save()
+            return render(request, 'profile.html')
+    else:
+        form2 = EditUser(instance=current_user)
+        form1 = EditProfile(instance=current_profile)
+    return render(request, 'edit_profile.html', {'form1': form1, 'form2': form2})
 
-    model = ImagerProfile
-    fields = ['location', 'camera_type']
-    success_url = '/profile/'
-    template_name = 'edit_profile.html'
 
-    def get_object(self):
-        """Override get_object method."""
-        return get_object_or_404(User, pk=self.request.user.id)
+class EditUser(ModelForm):
+    """Edit user form."""
+
+    class Meta:
+        """Meta."""
+
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
+
+
+class EditProfile(ModelForm):
+    """Edit profile form."""
+
+    class Meta:
+        """Meta."""
+
+        model = ImagerProfile
+        fields = ['location', 'camera_type']
+
+
+
 
 
