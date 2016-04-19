93709f82ef99adb5b2b0fd21ced788d41e6acaf2
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-04-18 20:22:09 -0700

    start of photo forms

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -6,6 +6,6 @@ from .views import photo_view, AddAlbum, AddPhoto
 urlpatterns = [
     url(r'^library$', Library, name='library'),
     url(r'^photos/(?P<user_id>[0-9]+)/(?P<photo_id>[0-9]+)', photo_view, name='photo_view'),
-    url(r'^albums/add$', AddAlbum.as_view(), name='add_album_view'),
+    # url(r'^albums/add$', AddAlbum.as_view(), name='add_album_view'),
     url(r'^photos/add$', AddPhoto.as_view(), name='add_photo_view'),
 ]
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.forms import ModelForm
+from .models import Photo
+from django.views.generic.edit import CreateView
 
 LIBRARY_TEMPLATE = 'library.html'
 
@@ -24,3 +27,38 @@ def photo_view(request, **kwargs):
     if image.published != 'public' or request.user.id != user.id:
         return HttpResponse('Unauthorized', status=401)
     return render(request, 'images/photo_view.html', context={'image': image})
+
+
+class AddPhoto(CreateView):
+    """View for form to add new photo."""
+
+    model = Photo
+    template_name = 'addphoto.html'
+    fields = ["title", "img_file", "description"]
+    success_url = 'library'
+
+
+class AddAlbum(ModelForm):
+    """View for form to add new album."""
+
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
