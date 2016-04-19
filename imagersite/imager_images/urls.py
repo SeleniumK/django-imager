ba091b906eb14445b2cf7c47438f56e2e34a4c26
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-04-18 19:32:19 -0700

    basic framework of create photos and albums views

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from imager_images.views import Library
-from .views import photo_view
+from .views import photo_view, AddAlbum, AddPhoto
 
 
 urlpatterns = [
     url(r'^library$', Library, name='library'),
-    url(r'^photos/(?P<user_id>[0-9]+)/(?P<photo_id>[0-9]+)', photo_view, name='photo_view')
+    url(r'^photos/(?P<user_id>[0-9]+)/(?P<photo_id>[0-9]+)', photo_view, name='photo_view'),
+    url(r'^albums/add$', AddAlbum.as_view(), name='add_album_view'),
+    url(r'^photos/add$', AddPhoto.as_view(), name='add_photo_view'),
 ]
